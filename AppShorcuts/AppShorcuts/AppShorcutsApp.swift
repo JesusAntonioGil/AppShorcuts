95d1049e791e1eb6312dbb0eae35073042d6489d
Jesús Antonio Gil <jesus.antoniogil@telefonica.com>
2025-03-02 13:21:22 +0100

    Create project

---
+++ AppShorcuts/AppShorcuts/AppShorcutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppShorcutsApp.swift
+//  AppShorcuts
+//
+//  Created by Jesus Antonio Gil on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct AppShorcutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppShorcuts/AppShorcuts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppShorcuts
+//
+//  Created by Jesus Antonio Gil on 2/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
