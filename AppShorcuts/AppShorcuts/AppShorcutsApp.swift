4d27281d02ac5a9bbfc25058e6e744f6d69145c4
Jesús Antonio Gil <jesus.antoniogil@telefonica.com>
2025-03-02 13:34:24 +0100

    Create Content View

--- AppShorcuts/AppShorcuts/AppShorcutsApp.swift
+++ AppShorcuts/AppShorcuts/AppShorcutsApp.swift
@@ -7,11 +7,13 @@
 
 import SwiftUI
 
+
 @main
 struct AppShorcutsApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .modelContainer(for: Memory.self)
         }
     }
 }
--- AppShorcuts/AppShorcuts/ContentView.swift
+++ AppShorcuts/AppShorcuts/ContentView.swift
@@ -6,19 +6,87 @@
 //
 
 import SwiftUI
+import SwiftData
+import AppIntents
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+
+@Model
+class Memory {
+    var caption: String
+    var date: Date
+    @Attribute(.externalStorage) var imageData: Data
+    
+    init(caption: String, date: Date, imageData: Data) {
+        self.caption = caption
+        self.date = date
+        self.imageData = imageData
+    }
+    
+    var uiImage:UIImage? {
+        .init(data: imageData)
     }
 }
 
+
+struct ContentView: View {
+    @Query(sort: [.init(\Memory.date, order: .reverse)], animation: .smooth)
+    var memories: [Memory]
+
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(memories) { memory in
+                    Section(memory.caption) {
+                        if let uiImage = memory.uiImage {
+                            Image(uiImage: uiImage)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                        }
+                    }
+                }
+            }
+            .listStyle(.insetGrouped)
+            .navigationTitle("Memories")
+            .overlay {
+                if memories.isEmpty {
+                    VStack {
+                        Text("No memories yet.")
+                            .font(.headline)
+                        
+                        Text("Tap the plus button to add your first memory.")
+                            .foregroundStyle(.secondary)
+                    }
+                    .padding(15)
+                }
+            }
+        }
+    }
+}
+
+
+
 #Preview {
     ContentView()
+}
+
+
+
+struct AddMemoryIntent: AppIntent {
+    static var title: LocalizedStringResource = "Add New Memory"
+    
+    @Parameter(
+        title: .init(stringLiteral: "Choose a Image"),
+        description: "Memory to be added",
+        supportedContentTypes: [.image],
+        inputConnectionBehavior: .connectToPreviousIntentResult
+    ) var imageFile: IntentFile
+    
+    @Parameter(title: "Caption") var caption: String
+    
+    
+    func perform() async throws -> some IntentResult {
+        print(caption)
+        return .result()
+    }
 }
